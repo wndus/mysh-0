a62cf12dd0ac010090bbf8154abd30809935a990
lee ju yeon <a01077956536@gmail.com>
2017-09-30 17:51:36 +0900

    to correct input cd pwd and implement

--- src/commands.c
+++ src/commands.c
@@ -1,12 +1,18 @@
 #include <string.h>
-
+#include<stdio.h>
 #include "commands.h"
 
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
 
-  // TODO: Fill it!
+  else{
+	int d;
+	d=chdir(argv[1]);
+	if(d==1) printf("Fail");
+	else printf("change");
+
+	}
   return 0;
 }
 
@@ -14,17 +20,21 @@ int do_pwd(int argc, char** argv) {
   if (!validate_pwd_argv(argc, argv))
     return -1;
 
-  // TODO: Fill it!
+  char *buf;
+  buf=getcwd(NULL,100);
+  printf("directory: %s",buf);
 
   return 0;
 }
 
 int validate_cd_argv(int argc, char** argv) {
-  // TODO: Fill it!
+  if((argc == 2 && strcmp(argv[0],"cd")==0) )  
   return 1;
+  else return 0;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-  // TODO: Fill it!
+  if(argc == 1 && strcmp(argv[0],"pwd")==0)
   return 1;
+  else return 0;
 }
